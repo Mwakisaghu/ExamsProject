37b83796bdff681effb5d56f8beed035045bb0e9
mwakisaghu <harrisonchambela2016@gmail.com>
2023-10-24 11:00:48 +0300

    This shows the updated CreateStudent class - retrieves the params from req body & parses

--- src/main/java/students/CreateStudent.java
+++ src/main/java/students/CreateStudent.java
@@ -1,14 +1,15 @@
 package students;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 import queries.QueryManager;
 
+
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,52 +22,59 @@ public class CreateStudent implements HttpHandler {
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        // Extract student information from the request
-        Map<String, Deque<String>> parameters = exchange.getQueryParameters();
-        String studentId = parameters.get("student_id").getFirst();
-        String firstName = parameters.get("first_name").getFirst();
-        String middleName = parameters.get("middle_name").getFirst();
-        String lastName = parameters.get("last_name").getFirst();
-        String classTierId = parameters.get("class_tier_id").getFirst();
-        String dateOfBirth = parameters.get("date_of_birth").getFirst();
-        String gender = parameters.get("gender").getFirst();
-        String emailAddress = parameters.get("email_address").getFirst();
+        // Extract student information from the request body
+        Map<String, Object> requestBodyMap = parseReqBody(exchange);
 
+        // Checking for the requestBody
+        if (requestBodyMap == null) {
+            // Bad Request
+            exchange.setStatusCode(400);
+            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+            exchange.getResponseSender().send("Error : Invalid request - request body not found");
+        }
+
+        // Sql Query
         String insertSql = "INSERT INTO students (student_id, first_name, middle_name, last_name, class_tier_id, date_of_birth, gender, email_address, created_at) " +
                 "VALUES (?, ?, ?, ?, ?, ?, ?, ?, current_timestamp)";
 
-        // Prepare the parameters for the insert operation
+        // Preparing the parameters for the insert operation
         Map<Integer, Object> insertMap = new HashMap<>();
-        insertMap.put(1, studentId);
-        insertMap.put(2, firstName);
-        insertMap.put(3, middleName);
-        insertMap.put(4, lastName);
-        insertMap.put(5, classTierId);
-        insertMap.put(6, dateOfBirth);
-        insertMap.put(7, gender);
-        insertMap.put(8, emailAddress);
+        assert requestBodyMap != null;
+        insertMap.put(1, requestBodyMap.get("student_id"));
+        insertMap.put(2, requestBodyMap.get("first_name"));
+        insertMap.put(3, requestBodyMap.get("middle_name"));
+        insertMap.put(4, requestBodyMap.get("last_name"));
+        insertMap.put(5, requestBodyMap.get("class_tier_id"));
+        insertMap.put(6, requestBodyMap.get("date_of_birth"));
+        insertMap.put(7, requestBodyMap.get("gender"));
+        insertMap.put(8, requestBodyMap.get("email_address"));
 
         try {
             // Executing the insert query & getting the generated keys and row count
-            int rowCount = QueryManager.executeInsertQuery(insertSql, insertMap);
+            QueryManager.executeInsertQuery(insertSql, insertMap);
 
-            if (rowCount > 0) {
-                // Status - Created
-                exchange.setStatusCode(201);
-                exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                exchange.getResponseSender().send("Student created successfully");
-            } else {
-                // Status - Internal Server Error
-                exchange.setStatusCode(500);
-                exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                exchange.getResponseSender().send("Student cannot be created");
-            }
+            // Status - Created
+            exchange.setStatusCode(201);
+            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+            exchange.getResponseSender().send("Student created successfully");
         } catch (SQLException e) {
             e.printStackTrace();
             // Status - Internal Server Error
             exchange.setStatusCode(500);
             exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
             exchange.getResponseSender().send("Internal Server Error");
+        }
+    }
+
+    // Parsing Json Request Body
+    private Map<String, Object> parseReqBody(HttpServerExchange exchange) throws IOException {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            byte[] reqBodyBytes = exchange.getInputStream().readAllBytes();
+            return objectMapper.readValue(reqBodyBytes, Map.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
