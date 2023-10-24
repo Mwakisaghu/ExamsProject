b955ef469b05a8f7b05ab8e47785907e8cac500e
mwakisaghu <harrisonchambela2016@gmail.com>
2023-10-24 11:02:10 +0300

    This shows the updated DeleteStudent class - req body parsed

--- src/main/java/students/DeleteStudent.java
+++ src/main/java/students/DeleteStudent.java
@@ -1,5 +1,6 @@
 package students;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
@@ -20,24 +21,27 @@ public class DeleteStudent implements HttpHandler {
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
         // Extracting the student ID from the request
-        String studentIdParam = exchange.getQueryParameters().get("studentId").getFirst();
+        Map<String, Object> requestBodyMap = parseRequestBody(exchange);
 
-        if (studentIdParam == null) {
+        if (requestBodyMap == null) {
             // Checking if the studentId is missing & send Bad Request
             exchange.setStatusCode(400);
+            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
             exchange.getResponseSender().send("Error: The studentId is missing");
             return;
         }
 
-        // executing the DELETE query - query runner
+        // Extracting the studentId from the request body
+        String studentId = (String) requestBodyMap.get("student_id");
+
+        // Sql query
         String deleteQuery = "DELETE FROM students WHERE student_id = ?";
+
+        // Preparing the parameters for the delete operation
         Map<Integer, Object> deleteMap = new HashMap<>();
+        deleteMap.put(1, studentId);
 
         try {
-            // Parsing the studentIdParam to an integer
-            int studentId = Integer.parseInt(studentIdParam);
-            deleteMap.put(1, studentId);
-
             int rowsDeleted = QueryManager.executeDeleteQuery(deleteQuery, deleteMap);
 
             // Delete the student's information
@@ -59,6 +63,17 @@ public class DeleteStudent implements HttpHandler {
             // Internal Server Error
             exchange.setStatusCode(500);
             exchange.getResponseSender().send("Error: Internal Server Error");
+        }
+    }
+
+    private Map<String, Object> parseRequestBody(HttpServerExchange exchange) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            byte[] reqBodyBytes = exchange.getInputStream().readAllBytes();
+            return objectMapper.readValue(reqBodyBytes, Map.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
