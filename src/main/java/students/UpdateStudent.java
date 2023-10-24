90800f41ba18c89e1b2b340ad0ec3fff3de5e9e2
mwakisaghu <harrisonchambela2016@gmail.com>
2023-10-24 11:14:50 +0300

    This shows the updated UpdateStudent class - Extracts the student data from the request body, parsing

--- src/main/java/students/UpdateStudent.java
+++ src/main/java/students/UpdateStudent.java
@@ -1,12 +1,13 @@
 package students;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 import queries.QueryManager;
 
 import java.sql.Connection;
-import java.util.Deque;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,51 +21,51 @@ public class UpdateStudent implements HttpHandler {
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        // Extracting the student ID and updating the date of birth from the request
-        Map<String, Deque<String>> parameters = exchange.getQueryParameters();
-        String studentIdParam = parameters.get("studentId").getFirst();
-        String updatedDateOfBirth = parameters.get("date_of_birth").getFirst();
+        // Extracting the student info
+        Map<String, Object> requestBodyMap = parseReqBody(exchange);
 
-        if (studentIdParam == null || updatedDateOfBirth == null) {
-            // Bad Request - if id param is missing
+        //Check for the request body
+        if (requestBodyMap == null) {
             exchange.setStatusCode(400);
-            exchange.getResponseSender().send(" Error : Missing studentId or date_of_birth parameters");
-            return;
+            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+            exchange.getResponseSender().send("Error : Invalid request - RequestBody Map not found");
         }
 
+        // Extracting the studentId from the request body
+        assert requestBodyMap != null;
+        String studentId = (String) requestBodyMap.get("student_id");
+
+        // Preparing the Sql statement
+        String updateSql = "UPDATE students SET date_of_birth = ? WHERE student_id = ?";
+
+        Map<Integer, Object> updateMap = new HashMap<>();
+        updateMap.put(1, requestBodyMap.get("date_of_birth"));
+        updateMap.put(2, studentId);
+
         try {
-            // Parsing the student ID to an integer
-            int studentId = Integer.parseInt(studentIdParam);
+            QueryManager.executeUpdateQuery(updateSql, updateMap);
 
-            // Update the info
-            String updateSql = "UPDATE students SET date_of_birth = ? WHERE student_id = ?";
-
-            Map<Integer, Object> updateMap = new HashMap<>();
-            updateMap.put(1, updatedDateOfBirth);
-            updateMap.put(2, studentId);
-
-            int rowCount = QueryManager.executeUpdateQuery(updateSql, updateMap);
-
-            // Updating the student's information
-            if (rowCount > 0) {
                 // success response - 200
                 exchange.setStatusCode(200);
                 exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
                 exchange.getResponseSender().send("Success: Student info updated successfully");
-            } else {
+            } catch (SQLException e) {
+                e.printStackTrace();
+
                 // if student with the provided ID not found
                 exchange.setStatusCode(404);
                 exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
                 exchange.getResponseSender().send("Error: Student not found");
             }
-        } catch (NumberFormatException e) {
-            // invalid student ID format & send a bad request
-            exchange.setStatusCode(400);
-            exchange.getResponseSender().send("Error: Invalid studentId parameter");
+    }
+    private Map<String, Object> parseReqBody(HttpServerExchange exchange) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            byte[] reqBodyBytes = exchange.getInputStream().readAllBytes();
+            return objectMapper.readValue(reqBodyBytes, Map.class);
         } catch (Exception e) {
-            // Internal Server Error
-            exchange.setStatusCode(500);
-            exchange.getResponseSender().send("Error : Internal Server Error");
+            e.printStackTrace();
+            return null;
         }
     }
 }
