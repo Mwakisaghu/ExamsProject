e0d5f78f3085988fef1e7f0632b95e08073aeeae
mwakisaghu <harrisonchambela2016@gmail.com>
2023-10-26 10:54:00 +0300

    This shows an updated path handler class

--- src/main/java/path/PathHandler.java
+++ src/main/java/path/PathHandler.java
@@ -1,19 +1,29 @@
 package path;
 
-import io.undertow.Handlers;
 import io.undertow.server.HttpHandler;
-import io.undertow.server.RoutingHandler;
+import io.undertow.server.HttpServerExchange;
 import routes.RoutesHandler;
 
-import static routes.RoutesHandler.createRoutesHandler;
+import java.sql.Connection;
 
-public class PathHandler {
-    public static HttpHandler create() {
-        io.undertow.server.handlers.PathHandler pathHandler = Handlers.path();
+public class PathHandler implements HttpHandler {
+    private final HttpHandler next;
 
-        // Mapping the root path to the RoutesHandler
-        pathHandler.addPrefixPath("/api", createRoutesHandler());
+    public PathHandler(HttpHandler next) {
+        this.next = next;
+    }
 
-        return pathHandler;
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        // Execute the next handler - RoutingHandler
+        next.handleRequest(exchange);
+    }
+
+    public static HttpHandler createPathHandler(Connection connection) {
+        // Creating the RoutingHandler - add the routes
+        HttpHandler routingHandler = RoutesHandler.createRoutingHandler();
+
+        // Wrap the routing handler in a custom PathHandler for pre,post-processing
+        return new PathHandler(routingHandler);
     }
 }
